1a66deaa0a8668952897d8ee3761145298318ac6
priya <priyapardeshi610@gmail.com>
2024-06-28 18:24:18 +0530

    second commit

--- project/app/urls.py
+++ project/app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path,include
 
 from .views import register
 
+
 from app import views
 urlpatterns = [
     # path('home/',home)        it show directly import
--- project/app/views.py
+++ project/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 from django.http import HttpResponse,JsonResponse
@@ -11,6 +11,9 @@ from django.http import HttpResponse,JsonResponse
 #     }
 #     return JsonResponse(data)
 
-def register (request):
-    return render(request, 'register.html')
+# def register (request):
+#     return render(request, 'register.html')
+
+def register(request):
+    return redirect("https://www.google.com/search?q=movies&sca_esv=7b0e25503c647052&sca_upv=1&rlz=1C1RXQR_enIN947IN947&sxsrf=ADLYWILhoUKh8C2HOr2ocMru2-mssWSW0g%3A1719576914857&ei=Uql-Zqv8M_ebseMPteeH2A8&ved=0ahUKEwir7bGZo_6GAxX3TWwGHbXzAfsQ4dUDCBA&uact=5&oq=movies&gs_lp=Egxnd3Mtd2l6LXNlcnAiBm1vdmllczIKECMYgAQYJxiKBTIKECMYgAQYJxiKBTILEAAYgAQYsQMYgwEyChAAGIAEGAoYywEyCxAAGIAEGLEDGIMBMgsQABiABBixAxiDATILEAAYgAQYsQMYgwEyCxAAGIAEGLEDGIMBMgsQABiABBixAxiDATIFEAAYgARItQ5Q_gVYgQlwAXgBkAEAmAG7AqABjQaqAQUyLTIuMbgBA8gBAPgBAZgCAqACngLCAgoQABiwAxjWBBhHwgINEAAYgAQYsAMYQxiKBZgDAIgGAZAGCpIHBTEuMC4xoAekEw&sclient=gws-wiz-serp")
 
