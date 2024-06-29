a704cdbf2874241c599ea7b6321e70b89af01f1b
priya <priyapardeshi610@gmail.com>
2024-06-29 18:41:22 +0530

    third commit

--- project/app/urls.py
+++ project/app/urls.py
@@ -2,10 +2,11 @@
 from django.urls import path,include
 
 from .views import register
-
+from .views import registerdata
 
 from app import views
 urlpatterns = [
     # path('home/',home)        it show directly import
-    path('register/', views.register)
+    path('register/', views.register ),
+    path('registerdata/', views.registerdata,name='register')
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -11,9 +11,19 @@ from django.http import HttpResponse,JsonResponse
 #     }
 #     return JsonResponse(data)
 
-# def register (request):
-#     return render(request, 'register.html')
+
 
 def register(request):
-    return redirect("https://www.google.com/search?q=movies&sca_esv=7b0e25503c647052&sca_upv=1&rlz=1C1RXQR_enIN947IN947&sxsrf=ADLYWILhoUKh8C2HOr2ocMru2-mssWSW0g%3A1719576914857&ei=Uql-Zqv8M_ebseMPteeH2A8&ved=0ahUKEwir7bGZo_6GAxX3TWwGHbXzAfsQ4dUDCBA&uact=5&oq=movies&gs_lp=Egxnd3Mtd2l6LXNlcnAiBm1vdmllczIKECMYgAQYJxiKBTIKECMYgAQYJxiKBTILEAAYgAQYsQMYgwEyChAAGIAEGAoYywEyCxAAGIAEGLEDGIMBMgsQABiABBixAxiDATILEAAYgAQYsQMYgwEyCxAAGIAEGLEDGIMBMgsQABiABBixAxiDATIFEAAYgARItQ5Q_gVYgQlwAXgBkAEAmAG7AqABjQaqAQUyLTIuMbgBA8gBAPgBAZgCAqACngLCAgoQABiwAxjWBBhHwgINEAAYgAQYsAMYQxiKBZgDAIgGAZAGCpIHBTEuMC4xoAekEw&sclient=gws-wiz-serp")
+    return render(request, 'register.html')
+
+def registerdata (request):
+    print(request.POST)
+
+
+
+
+
+
+# def register(request):
+#     return redirect("https://www.google.com/search?q=movies&sca_esv=7b0e25503c647052&sca_upv=1&rlz=1C1RXQR_enIN947IN947&sxsrf=ADLYWILhoUKh8C2HOr2ocMru2-mssWSW0g%3A1719576914857&ei=Uql-Zqv8M_ebseMPteeH2A8&ved=0ahUKEwir7bGZo_6GAxX3TWwGHbXzAfsQ4dUDCBA&uact=5&oq=movies&gs_lp=Egxnd3Mtd2l6LXNlcnAiBm1vdmllczIKECMYgAQYJxiKBTIKECMYgAQYJxiKBTILEAAYgAQYsQMYgwEyChAAGIAEGAoYywEyCxAAGIAEGLEDGIMBMgsQABiABBixAxiDATILEAAYgAQYsQMYgwEyCxAAGIAEGLEDGIMBMgsQABiABBixAxiDATIFEAAYgARItQ5Q_gVYgQlwAXgBkAEAmAG7AqABjQaqAQUyLTIuMbgBA8gBAPgBAZgCAqACngLCAgoQABiwAxjWBBhHwgINEAAYgAQYsAMYQxiKBZgDAIgGAZAGCpIHBTEuMC4xoAekEw&sclient=gws-wiz-serp")
 
